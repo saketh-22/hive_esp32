4c8b4481ed50cebd4e2510dde702368806d1efb5
saketh-22 <psaketh48@gmail.com>
2023-09-20 15:05:01 +0200

    Added support to firebase for sensor logs

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # Files to deploy esp32 for beehive monitoring and disease control
 
 - main.cpp file in src directory
+
+ ```
+# ESP32 Beehive Monitoring and Disease Control
+
+This repository contains the files necessary to deploy an ESP32 microcontroller for beehive monitoring and disease control. 
+
+## Requirements
+
+* ESP32 development board
+* Arduino IDE
+* DHT22 temperature and humidity sensor and/or OneWire DS18B20 Temperature sensor
+* 12V power supply
+* Breadboard and jumper wires
+* This code also supports a heater connected in the hive to maintain the temperatures. (Optional!)
+
+## Setup
+
+1. Install the Arduino IDE.
+2. Connect the ESP32 development board to your computer via USB.
+3. Select the appropriate board and port in the Arduino IDE.
+4. Clone this repository into your Arduino IDE projects directory.
+5. Open the `main.cpp` file in the Arduino IDE.
+6. Enter your WiFi SSID and password in the `setup()` function.
+7. Enter the IP address of your MQTT broker in the `setup()` function.
+8. Enter the topic you want to publish data to in the `setup()` function.
+9. Connect the  DHT22 sensor to the ESP32 board according to the following diagram:
+
+```
+DHT11 or DHT22  ESP32
+VCC             3.3V
+GND             GND
+DATA            D4
+```
+
+10. Connect the DS18B20 temp. sensor to the ESP32 board according to the following diagram:
+
+```
+DS18B20       ESP32
+VCC             3.3V
+GND             GND
+Data             5
+```
+
+
+12. Connect the 12V power supply to the ESP32 board.
+
+## Usage
+
+1. Open the Arduino IDE.
+2. Click the "Upload" button to compile and upload the code to the ESP32 board.
+3. Once the code has been uploaded, open the Serial Monitor.
+4. You should see the ESP32 board print out its IP address and the MQTT broker IP address.
+5. You should also see the ESP32 board print out the temperature, humidity, and air quality data.
+6. You 
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,48 @@
-#include <Arduino.h>
 
-// #include "DHTesp.h"
+#include <Arduino.h>
+#include <ArduinoWebsockets.h>
 #include "OneWire.h"
 #include "DallasTemperature.h"
 #include "WiFi.h"
-#include "WiFiClientSecure.h"
-#include "PubSubClient.h"
-#include "secrets.h"
+// #include "secrets.h"
 #include <ArduinoJson.h>
-
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <Firebase_ESP_Client.h>
+#include "time.h"
+#include <string>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-#define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+// #define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
+// #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
 
 #define DHTPIN 4      // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22 // DHT 22 (AM2302)
 
-float h, tc, tf;
-float t;
+float h_dht, tc, tf;
+float t_dht;
 
-WiFiClientSecure net = WiFiClientSecure();
-PubSubClient client(net);
+const char* WIFI_SSID = ""; //Enter SSID
+const char* WIFI_PASSWORD = ""; //Enter Password
+const char* websockets_server_host = ""; //Enter server adress
+const uint16_t websockets_server_port = 8080; // Enter server port
+const long utcOffsetInSeconds = 3600; // Central European Time (CET)
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+// char path[] = "/";
+// char host[] = "bee-service.onrender.com";
+
+using namespace websockets;
+
+WebsocketsClient client;
+// WiFiClient client;
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -42,118 +61,177 @@ OneWire oneWire(oneWireBus);
 // Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
 
+String server_data;
+
+// Insert Firebase project API Key
+#define API_KEY ""
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL ""
+#define USER_PASSWORD ""
+
+// Insert RTDB URLefine the RTDB URL
+#define DATABASE_URL ""
+// Define Firebase objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// Variable to save USER UID
+String uid;
+
+// Database main path (to be updated in setup with the user UID)
+String databasePath;
+// Database child nodes
+String tempDsbPath = "/temperatureDsb";
+String tempDhtPath = "/temperatureDht";
+String humPath = "/humidity";
+String timePath = "/timestamp";
+
+// Parent Node (to be updated in every loop)
+String parentPath;
+
+String timestamp;
+FirebaseJson json;
+
+
+// Timer variables (send new readings every three minutes)
+unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = 60000;
+
 // functions declaration
 void heater();
 float dsbtemp();
-void messageHandler(char *topic, byte *payload, unsigned int length);
+// void messageHandler(char *topic, byte *payload, unsigned int length);
 
-void connectAWS()
+// void messageHandler(char *topic, byte *payload, unsigned int length)
+// {
+//   Serial.print("incoming: ");
+//   Serial.println(topic);
+//   StaticJsonDocument<200> doc;
+//   deserializeJson(doc, payload);
+//   const char *message = doc["message"];
+//   Serial.println(message);
+// }
+
+#define Time_To_Sleep 10   //Time ESP32 will go to sleep (in seconds)
+#define S_To_uS_Factor 1000000ULL      //Conversion factor for micro seconds to seconds 
+
+void firebaseConnect() {
+  // Assign the api key (required)
+  config.api_key = API_KEY;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid + "/readings";
+
+}
+
+String getTime() {
+  // Initialize NTP client
+  
+  timeClient.begin();
+  timeClient.update();
+
+  // Get the current time
+  String timeStamp = timeClient.getFormattedTime();
+  return timeStamp;
+}
+
+void sendToFirebase(const std::string& tempDsb, const std::string& tempDht, const std::string& humDht) {
+  // Send new readings to database
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+
+    //Get current timestamp
+    timestamp = getTime();
+    Serial.print ("time: ");
+    Serial.println (timestamp);
+
+    parentPath= databasePath + "/" + String(timestamp);
+
+    json.set(tempDsbPath.c_str(), tempDsb);
+    json.set(tempDhtPath.c_str(), tempDht);
+    json.set(humPath.c_str(), humDht);
+    json.set(timePath, String(timestamp));
+    Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+  }
+}
+void setup()
 {
-  WiFi.mode(WIFI_STA);
+  Serial.begin(115200);
+
+  esp_sleep_enable_timer_wakeup(Time_To_Sleep * S_To_uS_Factor);
+  Serial.println("Setup ESP32 to sleep for every " + String(Time_To_Sleep) + " Seconds");
+  
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("Connecting to Wi-Fi");
 
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
     Serial.print(".");
+    delay(1000);
   }
 
-  // Configure WiFiClientSecure to use the AWS IoT device credentials
-  net.setCACert(AWS_CERT_CA);
-  net.setCertificate(AWS_CERT_CRT);
-  net.setPrivateKey(AWS_CERT_PRIVATE);
-
-  // Connect to the MQTT broker on the AWS endpoint we defined earlier
-  client.setServer(AWS_IOT_ENDPOINT, 8883);
-
-  // Create a message handler
-
-  client.setCallback(messageHandler);
-
-  Serial.println("Connecting to AWS IOT");
-
-  while (!client.connect(THINGNAME))
-  {
-    Serial.print(".");
-    delay(100);
+  if(WiFi.status() == WL_CONNECTED) {
+    Serial.print("Wifi Connected !!!");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
   }
+  //connectAWS();
+  
+  firebaseConnect();
 
-  if (!client.connected())
-  {
-    Serial.println("AWS IoT Timeout!");
-    return;
+  Serial.println("Connected to Wifi, Connecting to server.");
+  // try to connect to Websockets server
+  bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
+  if(connected) {
+      Serial.println("Connected!");
+      client.send("Hello Server");
+  } else {
+      Serial.println("Not Connected!");
   }
+  // run callback when messages are received
+  client.onMessage([&](WebsocketsMessage message){
+      Serial.print("Got Message: ");
+      Serial.println(message.data());
+      server_data = message.data();
+      Serial.print("ServerData: ");
+      Serial.print(server_data);
 
-  // Subscribe to a topic
-  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+  });
 
-  Serial.println("AWS IoT Connected!");
-}
-
-void publishMessage()
-{
-  StaticJsonDocument<200> doc;
-  doc["Sensor"] = "DHT22";
-  doc["humidity"] = h;
-  doc["temperature"] = t;
-  char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer); // print to client
-
-  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
-}
-
-void messageHandler(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("incoming: ");
-  Serial.println(topic);
-
-  StaticJsonDocument<200> doc;
-  deserializeJson(doc, payload);
-  const char *message = doc["message"];
-  Serial.println(message);
-}
-
-void readDht()
-{
-  // Delay between measurements.
-  delay(delayMS);
-  // Get temperature event and print its value.
-  Serial.println("#####DHT22 READINGS########");
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature))
-  {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else
-  {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-    t = event.temperature;
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity))
-  {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else
-  {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
-    h = event.relative_humidity;
-  }
-  Serial.println(" ");
-}
-
-void setup()
-{
-  Serial.begin(115200);
-  connectAWS();
+  
   pinMode(ledPin, OUTPUT);
 
   // dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
@@ -166,31 +244,103 @@ void setup()
 
 void loop()
 {
+  // get websocket msgs
+  if(client.available()) {
+        client.poll(); 
+    }else {
+      bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
+      if(connected) {
+          Serial.println("Connected!");
+          client.send("Hello Server");
+      } else {
+          Serial.println("Not Connected!");
+      }
+    }
+  
+  
   // % fetch detection data
   // if mite-detection=true --> start heat treatment
-  //
-  bool treatmentstatus = false; // fetch from cloud/user
 
-  if (treatmentstatus == true)
+  // DHT22 data
+  // readDht();
+  Serial.println("#####DHT22 READINGS########");
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature))
   {
-    heater();
+    Serial.println(F("Error reading temperature!"));
   }
   else
   {
-    // heater off
-    digitalWrite(ledPin, LOW);
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+    t_dht = event.temperature;
   }
-
-  // DHT22 data
-  readDht();
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else
+  {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+    h_dht = event.relative_humidity;
+  }
+  Serial.println(" ");
+// End DHT22 reading ........................................................................
 
   float dsbT = dsbtemp();
-
   Serial.println("#################");
-  publishMessage();
-  client.loop();
-  delay(5000); // Wait for a new reading from the sensor (DHT22 has ~0.5Hz sample rate)
+  Serial.println("Temp from OneWire sensor :");
+  Serial.println(dsbT);
+// End onew wire reading ..................................................................... 
+
+sendToFirebase(std::to_string(dsbT),std::to_string(t_dht),std::to_string(h_dht));
+
+// condition for heating process 
+  if (dsbT >= 29) //Here 35 is adjusted as trigger to proceed to AI detection algorithm
+  {
+   // WebSocket setup
+   if (client.available()) {
+   client.send("Capture");
+   Serial.print("Sent capture signal to Server");
+   while(true){
+    // webSocketClient.getData(server_data);
+    if(client.available()) {
+        client.poll(); 
+    }
+    Serial.print("received response:");
+    Serial.print(server_data);
+    if(server_data == "0" || server_data == "1")
+    {break;}
+    
+    }
+      
+   }
+      // Now that we have received the response, act on the treatment status
+      
+  String treatmentstatus = server_data;
+  if (treatmentstatus == "0") {
+    // Treatment status is false
+     Serial.print("No disease found");
+  } else {
+    // Treatment status is true
+    Serial.print("Disease present, Starting treatment");
+    //heater();
+    delay(50000);
+    Serial.print("Treatment finished");
+  }
+    
+  
 }
+// delay(5000); // Wait for a new reading from the sensor (DHT22 has ~0.5Hz sample rate)
+esp_deep_sleep_start();
+}
+
 
 void heater()
 {
